088930286694e05335b028f3ed8db535613319c4
Luke Codewalker <arathorn089@arcor.de>
2018-08-30 11:29:04 +0200

    finished data formatting and insertion

--- scraping.py
+++ scraping.py
@@ -26,43 +26,50 @@ else:
 
 # get the wikipedia page and parse the content
 response = requests.get(
-    'https://de.m.wikipedia.org/wiki/Liste_von_Bergwerken_in_Nordrhein-Westfalen')
+    "https://de.m.wikipedia.org/wiki/Liste_von_Bergwerken_in_Nordrhein-Westfalen")
 soup = BeautifulSoup(response.text, 'html.parser')
 
 print('Now sraping', soup.title.get_text())
 
 # find all collapsible divs and extract the tables in there
-divs = soup.find_all('div', {'class': 'collapsible-block'})
+divs = soup.find_all("div", {"class": "collapsible-block"})
 
 tables = []
 for div in divs:
-    table = div.find('table')
+    table = div.find("table")
     if(table):
         tables.append(table)
 
 # pull data out of tables and write it to db
+print("Formatting data and inserting into", tablename)
+
 for i, table in enumerate(tables):
-    rows = table.find_all('tr')
+    rows = table.find_all("tr")
     for row in rows[1:len(rows)-1]:
         # make list of all elements in row
-        raw_data = list(BeautifulSoup.get_text(x).replace('\n', '').replace('\'', '')
-                        for x in row.find_all('td'))
+        raw_data = list(BeautifulSoup.get_text(x).replace("\n", "").replace("\'", "")
+                        for x in row.find_all("td"))
         # turn list into dictionary and format values correctly
+        open_year_match = re.search(r"\d{4}", raw_data[3])
+        close_year_match = re.search(r"\d{4}", raw_data[4])
         data = {
             "name": raw_data[0],
             "region": raw_data[1],
             "city": raw_data[2],
-            "open_year": re.search(r'\d{4}', raw_data[3]).group(0),
-            "close_year": re.search(r'\d{4}', raw_data[4]).group(0),
-            "notes": raw_data[5],
-            "is_active": False}
+            "open_year": open_year_match.group(0) if open_year_match else "NULL",
+            "close_year": close_year_match.group(0) if close_year_match else "NULL",
+            "notes": raw_data[5]
+        }
+
+        data["is_active"] = True if raw_data[4] == "heute" or data["notes"] == "aktiv" else False
+
         # insert into db
-        print(
-            "VALUES('{name}', '{region}', '{city}', '{open_year}', '{close_year}', '{is_active}', '{notes}')".format(**data))
-        #cursor.execute("INSERT INTO wikidata (name, region, city, open_year, close_year, notes) VALUES ('{0}', '{1}', '{2}', '{3}','{4}', '{5}');".format(*data))
+        cursor.execute(
+            "INSERT INTO wikidata (name, region, city, open_year, close_year, is_active, notes) VALUES ('{name}', '{region}', '{city}', {open_year}, {close_year}, '{is_active}', '{notes}');".format(**data))
 
 # commit changes and close connections
+print("Done")
+
 connection.commit()
 cursor.close()
 connection.close()
-print('Done')
