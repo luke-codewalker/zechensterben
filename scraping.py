ba77cca393f97a2e939a9366351bb467b1996143
Luke Codewalker <arathorn089@arcor.de>
2018-08-30 10:53:22 +0200

    added proper date extraction to conform with int type in db

--- scraping.py
+++ scraping.py
@@ -1,7 +1,7 @@
 import psycopg2
 import requests
 from bs4 import BeautifulSoup
-
+import re
 
 # connect to db and open a cursor to start read/write
 connection = psycopg2.connect(
@@ -40,16 +40,29 @@ for div in divs:
     if(table):
         tables.append(table)
 
-# pull data out of html tables and write them to db
+# pull data out of tables and write it to db
 for i, table in enumerate(tables):
     rows = table.find_all('tr')
     for row in rows[1:len(rows)-1]:
-        data = list(BeautifulSoup.get_text(x).replace('\n', '').replace('\'','')
-                    for x in row.find_all('td'))
-        cursor.execute(
-            "INSERT INTO wikidata (name, region, city, open_year, close_year, notes) VALUES ('{0}', '{1}', '{2}', '{3}','{4}', '{5}');".format(*data))
+        # make list of all elements in row
+        raw_data = list(BeautifulSoup.get_text(x).replace('\n', '').replace('\'', '')
+                        for x in row.find_all('td'))
+        # turn list into dictionary and format values correctly
+        data = {
+            "name": raw_data[0],
+            "region": raw_data[1],
+            "city": raw_data[2],
+            "open_year": re.search(r'\d{4}', raw_data[3]).group(0),
+            "close_year": re.search(r'\d{4}', raw_data[4]).group(0),
+            "notes": raw_data[5],
+            "is_active": False}
+        # insert into db
+        print(
+            "VALUES('{name}', '{region}', '{city}', '{open_year}', '{close_year}', '{is_active}', '{notes}')".format(**data))
+        #cursor.execute("INSERT INTO wikidata (name, region, city, open_year, close_year, notes) VALUES ('{0}', '{1}', '{2}', '{3}','{4}', '{5}');".format(*data))
 
 # commit changes and close connections
 connection.commit()
 cursor.close()
 connection.close()
+print('Done')
