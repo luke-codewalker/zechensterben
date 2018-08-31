0419b04fe3235853836fff48b53e6268ec377e60
Luke Codewalker <arathorn089@arcor.de>
2018-08-31 21:34:50 +0200

    added wikipedia artivle urls for zechen

--- 01_scraping/scraping.py
+++ 01_scraping/scraping.py
@@ -26,7 +26,7 @@ result = cursor.fetchone()
 if(result == None):
     print("--> Creating new table", tablename)
     cursor.execute(
-        "CREATE TABLE {} (id varchar PRIMARY KEY, name varchar, region varchar, city varchar, open_year integer, close_year integer, is_active boolean DEFAULT false, notes varchar, lon float, lat float);".format(tablename))
+        "CREATE TABLE {} (id varchar PRIMARY KEY, name varchar, region varchar, city varchar, open_year integer, close_year integer, is_active boolean DEFAULT false, notes varchar, wiki_url varchar, lon float, lat float);".format(tablename))
 else:
     print("--> Using existing table", result[2])
 
@@ -60,28 +60,34 @@ inserted_ids = []
 for i, table in enumerate(tables):
     rows = table.find_all("tr")
     for row in rows[1:len(rows)-1]:
-        # make list of all elements in row
-        raw_data = list(BeautifulSoup.get_text(x).replace("\n", "").replace("\'", "")
+        # make list of text of all elements in row
+        raw_text = list(BeautifulSoup.get_text(x).replace("\n", " ").replace("\'", "")
                         for x in row.find_all("td"))
+
+        # additionally extract the article links (links with classes are edit links etc.)
+        anchor = row.find("td").find_all("a", class_="")
+        url = anchor[0].get("href") if anchor else None
+
         # turn list into dictionary and format values correctly
-        open_year_match = re.search(r"\d{4}", raw_data[3])
-        close_year_match = re.search(r"\d{4}", raw_data[4])
+        open_year_match = re.search(r"\d{4}", raw_text[3])
+        close_year_match = re.search(r"\d{4}", raw_text[4])
         data = {
-            "name": raw_data[0],
-            "region": raw_data[1],
-            "city": raw_data[2],
+            "name": raw_text[0],
+            "region": raw_text[1],
+            "city": raw_text[2],
             "open_year": open_year_match.group(0) if open_year_match else None,
             "close_year": close_year_match.group(0) if close_year_match else None,
-            "notes": raw_data[5]
+            "notes": raw_text[5],
+            "wiki_url": "https://de.wikipedia.org" + url if url else None
         }
 
         ident_string = data["name"] + data["region"] + data["city"]
         data["id"] = md5(ident_string.encode("utf-8")).hexdigest()[0:7]
-        data["is_active"] = True if raw_data[4] == "heute" or data["notes"] == "aktiv" else False
+        data["is_active"] = True if raw_text[4] == "heute" or data["notes"] == "aktiv" else False
 
         # insert into db (on succes id is returned and added to list)
         cursor.execute(
-            "INSERT INTO wikidata (id, name, region, city, open_year, close_year, is_active, notes) VALUES (%(id)s, %(name)s, %(region)s, %(city)s, %(open_year)s::integer, %(close_year)s::integer, %(is_active)s::boolean, %(notes)s) ON CONFLICT (id) DO NOTHING RETURNING id;", data)
+            "INSERT INTO wikidata (id, name, region, city, open_year, close_year, is_active, notes, wiki_url) VALUES (%(id)s, %(name)s, %(region)s, %(city)s, %(open_year)s::integer, %(close_year)s::integer, %(is_active)s::boolean, %(notes)s, %(wiki_url)s) ON CONFLICT (id) DO NOTHING RETURNING id;", data)
         if(cursor.fetchone()):
             inserted_ids.append(cursor.fetchone())
 
