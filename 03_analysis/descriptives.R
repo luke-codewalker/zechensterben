d566c546009e2a8938b189b2cd901800c85ed587
Luke Codewalker <arathorn089@arcor.de>
2018-08-31 19:19:20 +0200

    added leaflet map to R analysis

--- 03_analysis/descriptives.R
+++ 03_analysis/descriptives.R
@@ -3,45 +3,54 @@ rm(list=ls())
 readRenviron(paste0(getwd(), "/.env"))
 
 # load (or install if necessary) postgreSQL package
-if(!require("RPostgreSQL", quietly=TRUE)) {
-  install.packages("RPostgreSQL")
-  library(RPostgreSQL)
+packages <- c("RPostgreSQL", "leaflet")
+for (package in packages) {
+  if(!require(package, quietly=TRUE)) {
+    install.packages(package)
+    library(package, character.only = TRUE)
+  }
 }
 
 # connect to postgreSQL
-driver <- dbDriver("PostgreSQL")
-connection <- dbConnect(driver,
-                       host = "localhost",
-                       port = 5432,
-                       dbname = Sys.getenv("DB_NAME"),
-                       user = Sys.getenv("DB_USER"),
-                       password = Sys.getenv("DB_PASSWORD")
-                       )
+connection <- dbDriver("PostgreSQL") %>% 
+  dbConnect(.,
+           host = "localhost",
+           port = 5432,
+           dbname = Sys.getenv("DB_NAME"),
+           user = Sys.getenv("DB_USER"),
+           password = Sys.getenv("DB_PASSWORD")
+           )
 
 # load wikidata table into dataframe
-data <- dbGetQuery(connection, "SELECT * FROM wikidata WHERE open_year IS NOT NULL AND (close_year IS NOT NULL OR is_active = True);")
+df <- dbGetQuery(connection, "SELECT * FROM wikidata WHERE open_year IS NOT NULL AND (close_year IS NOT NULL OR is_active = True);")
 
 # substitute close year with current year for active mines
-data$close_year[data$is_active==TRUE] <- 2018
+df$close_year[df$is_active==TRUE] <- 2018
 
 # how many are still active?
-sum(as.integer(data$is_active==TRUE))
+sum(as.integer(df$is_active==TRUE))
 
 # descriptives for closing and opening years
-(openStats <- summary(data$open_year))
-(closeStats <- summary(data$close_year))
+(openStats <- summary(df$open_year))
+(closeStats <- summary(df$close_year))
 
 # count active shafts for each year and plot
 year <- openStats["Min."]:closeStats["Max."]
 num <- sapply(year, function(x) {
-  v <- logical(nrow(data))
-  for(i in 1:nrow(data)) {
-    v[i] <- x > data$open_year[i] && x < data$close_year[i]
+  v <- logical(nrow(df))
+  for(i in 1:nrow(df)) {
+    v[i] <- x > df$open_year[i] && x < df$close_year[i]
   }
   return(sum(as.integer(v)))
 })
 
 plot(num ~ year, type="l")
+
+# plot locations
+map <- leaflet(df) %>%
+  addTiles() %>%
+  addCircleMarkers(lng=~lon, lat=~lat, popup=~name, clusterOptions = markerClusterOptions())
+map
 
 # close the connection
 dbDisconnect(connection)
